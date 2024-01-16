726049e50811a991b5a8468416bad13eacd27bcd
gaplin <gaplinn@gmail.com>
2024-01-16 21:23:10 +0100

    6

---
+++ 6/day6p1.py
@@ -0,0 +1,32 @@
+import re
+
+banks = [int(x) for x in re.findall(r'\d+', open('input2.txt').read().strip())]
+n = len(banks)
+
+def redistribute(banks: list, n: int) -> None:
+    max_b, max_idx = -1, -1
+
+    for i in range(n):
+        if banks[i] > max_b:
+            max_b = banks[i]
+            max_idx = i
+
+    banks[max_idx] = 0
+    i = (max_idx + 1) % n
+    while max_b > 0:
+        banks[i] += 1
+        max_b -= 1
+        i = (i + 1) % n
+
+bank_cache = {tuple(banks)}
+
+i = 0
+while True:
+    redistribute(banks, n)
+    i += 1
+    key = tuple(banks)
+    if key in bank_cache:
+        break
+    bank_cache.add(key)
+
+print(i)
