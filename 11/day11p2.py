51ccbb0ed1c5c38052c3a85dc4af57a36d4954ff
gaplin <gaplinn@gmail.com>
2024-01-18 18:06:15 +0100

    11

---
+++ 11/day11p2.py
@@ -0,0 +1,22 @@
+from queue import Queue
+
+directions = {
+    'n': (0, 2),
+    's': (0, -2),
+    'nw': (-1, 1),
+    'ne': (1, 1),
+    'sw': (-1, -1),
+    'se': (1, -1)
+}
+
+moves = open('input2.txt').read().strip().split(',')
+
+position = [0, 0]
+result = 0
+for move in moves:
+    dx, dy = directions[move]
+    position[0] += dx
+    position[1] += dy
+    result = max(result, (abs(position[0]) + abs(position[1])) // 2)
+
+print(result)
